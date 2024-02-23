7ad4fb223bf9238c312f2b9c1eee82ad5f7ed2bd
delmy <dfajardob@miumg.edu.gt>
2024-02-23 17:23:51 -0600

    mis programas

---
+++ LeerTeclado/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeerTeclado
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("suma de dos numeros");
+            Console.WriteLine("Ingresa numero 1: ");
+            //los datos se leen como cadena
+            string dato = Console.ReadLine();
+            //convertir a numero con int.Parse
+            int n1 = int.Parse(dato);
+            Console.WriteLine("inggresa numero 2: ");
+            dato = Console.ReadLine();
+            int n2 = int.Parse(dato);
+            int suma = n1 + n2;
+            Console.WriteLine("el resultaod es: {0}", suma);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ programas/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fundamentos 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hazlo facil, haz click!");
+            Console.ReadLine();
+
+
+        }
+    }
+}
